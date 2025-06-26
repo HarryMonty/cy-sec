6de0ba8e45accfc7404512bee99b62a1c2f34d41
Harry Monty <harrymonty@live.com.au>
2025-06-26 15:07:24 +1000

    sec-web-app update

--- beginner/secure-web-app/app/app.py
+++ beginner/secure-web-app/app/app.py
@@ -1,26 +1,38 @@
 from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SecretKey"
+bcrypt = Bcrypt(app)
 
 # Check username exists
-def check_user(name):
+def check_user(name, password):
     with open("app/static/database.db", "r") as file:
             for line in file:
                 users = (line.strip())
-                if users == name:
+                if users.replace("U:", "") == name:
                     return False
     with open("app/static/database.db", "a") as file:
-        file.write(f"{name}\n")
+        file.write(f"U:{name}\n")
+
+        pw_hash = bcrypt.generate_password_hash(password)
+        file.write(f"P:{pw_hash}\n")
+        print(bcrypt.check_password_hash(pw_hash, password))
         return True
 
 # Register Form
 class RegisterForm(FlaskForm):
     name = StringField("Username", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Login Form
+class LoginForm(FlaskForm):
+    name = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # HTML Routes
@@ -37,7 +49,7 @@ def register():
     if form.validate_on_submit():
         name = form.name.data
         password = form.password.data
-        if check_user(name):
+        if check_user(name, password):
             return render_template("login.html")
         else:
             form.name.data = ''
